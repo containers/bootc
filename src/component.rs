644db684e19b1ae4a4b8eca547c1277433313514
Colin Walters <walters@verbum.org>
2020-10-02 17:40:06 -0400

    component: Add a lot of docstrings
    On general principle but also prep for adding a new method.

--- src/component.rs
+++ src/component.rs
@@ -21,19 +21,37 @@ pub(crate) enum ValidationResult {
 
 /// A component along with a possible update
 pub(crate) trait Component {
+    /// Returns the name of the component; this will be used for serialization
+    /// and should remain stable.
     fn name(&self) -> &'static str;
 
+    /// Implementation of `bootupd install` for a given component.  This should
+    /// gather data (or run binaries) from the source root, and install them
+    /// into the target root.  It is expected that sub-partitions (e.g. the ESP)
+    /// are mounted at the expected place.  For operations that require a block device instead
+    /// of a filesystem root, the component should query the mount point to
+    /// determine the block device.
+    /// This will be run during a disk image build process.
     fn install(&self, src_root: &str, dest_root: &str) -> Result<InstalledContent>;
 
+    /// Implementation of `bootupd generate-update-metadata` for a given component.
+    /// This expects to be run during an "image update build" process.  For CoreOS
+    /// this is an `rpm-ostree compose tree` for example.  For a dual-partition
+    /// style updater, this would be run as part of a postprocessing step
+    /// while the filesystem for the partition is mounted.
     fn generate_update_metadata(&self, sysroot: &str) -> Result<ContentMetadata>;
 
+    /// Used on the client to query for an update cached in the current booted OS.
     fn query_update(&self) -> Result<Option<ContentMetadata>>;
 
+    /// Used on the client to run an update.
     fn run_update(&self, current: &InstalledContent) -> Result<InstalledContent>;
 
+    /// Used on the client to validate an installed version.
     fn validate(&self, current: &InstalledContent) -> Result<ValidationResult>;
 }
 
+/// Given a component name, create an implementation.
 pub(crate) fn new_from_name(name: &str) -> Result<Box<dyn Component>> {
     let r: Box<dyn Component> = match name {
         "EFI" => Box::new(crate::efi::EFI::default()),
@@ -42,12 +60,16 @@ pub(crate) fn new_from_name(name: &str) -> Result<Box<dyn Component>> {
     Ok(r)
 }
 
+/// Returns the path to the JSON file containing a component's available update metadata installed
+/// into the booted operating system root.
 pub(crate) fn component_update_metapath(sysroot: &str, component: &dyn Component) -> PathBuf {
     Path::new(sysroot)
         .join(BOOTUPD_UPDATES_DIR)
         .join(format!("{}.json", component.name()))
 }
 
+/// Returns the path to the payload directory for an available update for
+/// a component.
 pub(crate) fn component_updatedir(sysroot: &str, component: &dyn Component) -> PathBuf {
     Path::new(sysroot)
         .join(BOOTUPD_UPDATES_DIR)
@@ -67,6 +89,7 @@ pub(crate) fn write_update_metadata(
     Ok(())
 }
 
+/// Given a component, return metadata on the available update (if any)
 pub(crate) fn get_component_update(
     sysroot: &str,
     component: &dyn Component,
