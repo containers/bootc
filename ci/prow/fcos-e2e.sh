5ee548c2fde7daec4ba186838a92952c72667199
Colin Walters <walters@verbum.org>
2021-03-09 20:13:07 +0000

    prow: Fix overrides
    Not actually testing what you think you are is the worst.

--- ci/prow/fcos-e2e.sh
+++ ci/prow/fcos-e2e.sh
@@ -3,8 +3,6 @@ set -xeuo pipefail
 
 # Prow jobs don't support adding emptydir today
 export COSA_SKIP_OVERLAY=1
-gitdir=$(pwd)
-cd $(mktemp -d)
 cosa init --force https://github.com/coreos/fedora-coreos-config/
 cosa fetch
 cosa build
