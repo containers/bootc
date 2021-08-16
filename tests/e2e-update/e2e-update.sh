16312def35025842ff534690f896ea30a8e3ffa3
Colin Walters <walters@verbum.org>
2021-08-16 15:08:53 -0400

    tests/e2e-update: Bump to f34 grub
    Need to tweak this to auto-detect gold versus updates, or
    probably better just synthesize an update.

--- tests/e2e-update/e2e-update.sh
+++ tests/e2e-update/e2e-update.sh
@@ -79,13 +79,13 @@ undo_manifest_fork() {
 if test -z "${e2e_skip_build:-}"; then
     echo "Building starting image"
     rm -f ${overrides}/rpm/*.rpm
-    add_override grub2-2.04-22.fc32
+    add_override grub2-2.06-2.fc34
     runv cosa build
     prev_image=$(runv cosa meta --image-path qemu)
     create_manifest_fork
     rm -f ${overrides}/rpm/*.rpm
     echo "Building update ostree"
-    add_override grub2-2.04-23.fc32
+    add_override grub2-2.06~rc1-3.fc34
     mv ${test_tmpdir}/yumrepo/packages/$(arch)/*.rpm ${overrides}/rpm/
     # Only build ostree update
     runv cosa build ostree
