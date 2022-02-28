6c46a71d12f9e066f37119a339fcc707621e2776
Colin Walters <walters@verbum.org>
2022-02-28 09:59:08 -0500

    ci: Drop hack to build skopeo
    A new enough version is now in Fedora.

--- ostree-ext/ci/installdeps.sh
+++ ostree-ext/ci/installdeps.sh
@@ -4,14 +4,5 @@ set -xeuo pipefail
 # Always pull ostree from updates-testing to avoid the bodhi wait
 dnf -y --enablerepo=updates-testing update ostree-devel
 
-# Pull the code from https://github.com/containers/skopeo/pull/1476
-# if necessary.
-if ! skopeo experimental-image-proxy --help &>/dev/null; then
-    dnf -y install dnf-utils
-    dnf builddep -y skopeo
-    git clone --depth=1 https://github.com/containers/skopeo
-    cd skopeo
-    make
-    install -m 0755 bin/skopeo /usr/bin/
-fi
-
+# Our tests depend on this
+dnf -y install skopeo
