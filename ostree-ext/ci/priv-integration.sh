containers/bootc

commit 12444f6a525f745011cb05850881e85e8daa6ad5

deploy: Add an API to prune undeployed images
This is part of fixing https://github.com/coreos/rpm-ostree/issues/4391
but is also in the general theme of making things less "stateful".

A huge huge mess today is `rpm-ostree rebase` and `bootc switch`
both have `--retain` options which keep the previous image.

But really what we want is to use the deployments as source-of-truth;
that way if e.g. an admin pins a deployment, it automatically pins
the image too.

And this will help strongly align with the bootc direction in
reconciling to desired state.

ostree-ext/ci/priv-integration.sh
@@ -24,6 +24,8 @@ fi
 if test '!' -d "${sysroot}/ostree/deploy/${stateroot}"; then
     ostree admin os-init "${stateroot}" --sysroot "${sysroot}"
 fi
+# Should be no images pruned
+ostree-ext-cli container image prune-images --sysroot "${sysroot}"
 # Test the syntax which uses full imgrefs.
 ostree-ext-cli container image deploy --sysroot "${sysroot}" \
     --stateroot "${stateroot}" --imgref "${imgref}"
@@ -34,8 +36,11 @@ ostree admin --sysroot="${sysroot}" undeploy 0
 ostree-ext-cli container image deploy --transport registry --sysroot "${sysroot}" \
     --stateroot "${stateroot}" --image "${image}" --no-signature-verification
 ostree admin --sysroot="${sysroot}" status
-ostree-ext-cli container image remove --repo "${sysroot}/ostree/repo" registry:"${image}"
 ostree admin --sysroot="${sysroot}" undeploy 0
+# Now we should prune it
+ostree-ext-cli container image prune-images --sysroot "${sysroot}"
+ostree-ext-cli container image list --repo "${sysroot}/ostree/repo" > out.txt
+test $(stat -c '%s' out.txt) = 0
 
 for img in "${image}"; do
     ostree-ext-cli container image deploy --sysroot "${sysroot}" \

ostree-ext/lib/src/cli.rs
@@ -18,6 +18,7 @@ use crate::commit::container_commit;
 use crate::container::store::{ImportProgress, LayerProgress, PreparedImport};
 use crate::container::{self as ostree_container};
 use crate::container::{Config, ImageReference, OstreeImageReference};
+use crate::sysroot::SysrootLock;
 use ostree_container::store::{ImageImporter, PrepareResult};
 
 /// Parse an [`OstreeImageReference`] from a CLI arguemnt.
@@ -271,6 +272,17 @@ pub(crate) enum ContainerImageOpts {
         /// Path to the repository
         #[clap(long, value_parser)]
         repo: Utf8PathBuf,
+    },
+
+    /// Garbage collect unreferenced image layer references.
+    PruneImages {
+        /// Path to the system root
+        #[clap(long)]
+        sysroot: Utf8PathBuf,
+
+        #[clap(long)]
+        /// Also prune layers
+        and_layers: bool,
     },
 
     /// Perform initial deployment for a container image
@@ -823,6 +835,31 @@ where
                     let repo = parse_repo(&repo)?;
                     let nlayers = crate::container::store::gc_image_layers(&repo)?;
                     println!("Removed layers: {nlayers}");
+                    Ok(())
+                }
+                ContainerImageOpts::PruneImages {
+                    sysroot,
+                    and_layers,
+                } => {
+                    let sysroot = &ostree::Sysroot::new(Some(&gio::File::for_path(&sysroot)));
+                    sysroot.load(gio::Cancellable::NONE)?;
+                    let sysroot = &SysrootLock::new_from_sysroot(sysroot).await?;
+                    let removed = crate::container::deploy::remove_undeployed_images(sysroot)?;
+                    match removed.as_slice() {
+                        [] => {
+                            println!("No unreferenced images.");
+                            return Ok(());
+                        }
+                        o => {
+                            for imgref in o {
+                                println!("Removed: {imgref}");
+                            }
+                        }
+                    }
+                    if and_layers {
+                        let nlayers = crate::container::store::gc_image_layers(&sysroot.repo())?;
+                        println!("Removed layers: {nlayers}");
+                    }
                     Ok(())
                 }
                 ContainerImageOpts::Copy {

ostree-ext/lib/src/container/deploy.rs
@@ -1,8 +1,12 @@
 //! Perform initial setup for a container image based system root
 
+use std::collections::HashSet;
+
 use super::store::LayeredImageState;
-use super::OstreeImageReference;
+use super::{ImageReference, OstreeImageReference};
 use crate::container::store::PrepareResult;
+use crate::keyfileext::KeyFileExt;
+use crate::sysroot::SysrootLock;
 use anyhow::Result;
 use fn_error_context::context;
 use ostree::glib;
@@ -111,4 +115,50 @@ pub async fn deploy(
     }
 
     Ok(state)
+}
+
+/// Query the container image reference for a deployment
+fn deployment_origin_container(
+    deploy: &ostree::Deployment,
+) -> Result<Option<OstreeImageReference>> {
+    let origin = deploy
+        .origin()
+        .map(|o| o.optional_string("origin", ORIGIN_CONTAINER))
+        .transpose()?
+        .flatten();
+    let r = origin
+        .map(|v| OstreeImageReference::try_from(v.as_str()))
+        .transpose()?;
+    Ok(r)
+}
+
+/// Remove all container images which are not the target of a deployment.
+/// This acts equivalently to [`super::store::remove_images()`] - the underlying layers
+/// are not pruned.
+///
+/// The set of removed images is returned.
+pub fn remove_undeployed_images(sysroot: &SysrootLock) -> Result<Vec<ImageReference>> {
+    let repo = &sysroot.repo();
+    let deployment_origins: Result<HashSet<_>> = sysroot
+        .deployments()
+        .into_iter()
+        .filter_map(|deploy| {
+            deployment_origin_container(&deploy)
+                .map(|v| v.map(|v| v.imgref))
+                .transpose()
+        })
+        .collect();
+    let deployment_origins = deployment_origins?;
+    // TODO add an API that returns ImageReference instead
+    let all_images = super::store::list_images(&sysroot.repo())?
+        .into_iter()
+        .filter_map(|img| ImageReference::try_from(img.as_str()).ok());
+    let mut removed = Vec::new();
+    for image in all_images {
+        if !deployment_origins.contains(&image) {
+            super::store::remove_image(repo, &image)?;
+            removed.push(image);
+        }
+    }
+    Ok(removed)
 }

ostree-ext/lib/src/container/mod.rs
@@ -48,7 +48,7 @@ pub(crate) const COMPONENT_SEPARATOR: char = ',';
 type Result<T> = anyhow::Result<T>;
 
 /// A backend/transport for OCI/Docker images.
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Hash, Debug, PartialEq, Eq)]
 pub enum Transport {
     /// A remote Docker/OCI registry (`registry:` or `docker://`)
     Registry,
@@ -63,7 +63,7 @@ pub enum Transport {
 /// Combination of a remote image reference and transport.
 ///
 /// For example,
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct ImageReference {
     /// The storage and transport for the image
     pub transport: Transport,
@@ -72,7 +72,7 @@ pub struct ImageReference {
 }
 
 /// Policy for signature verification.
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum SignatureSource {
     /// Fetches will use the named ostree remote for signature verification of the ostree commit.
     OstreeRemote(String),
@@ -87,7 +87,7 @@ pub const LABEL_VERSION: &str = "version";
 
 /// Combination of a signature verification mechanism, and a standard container image reference.
 ///
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct OstreeImageReference {
     /// The signature verification mechanism.
     pub sigverify: SignatureSource,

commit 3aa5073aa530b211fe6d0b039d0490c1545a25e2

Add convenient API/CLI to prune all content
This is kind of a usability regression from
https://github.com/ostreedev/ostree-rs-ext/pull/498/commits/12444f6a525f745011cb05850881e85e8daa6ad5
"deploy: Add an API to prune undeployed images"

This new API and CLI make it convenient to prune the images,
the layers, *and* the underlying objects.

cc https://github.com/coreos/rpm-ostree/pull/4720

This API in particular is what bootc wants.  For rpm-ostree
we do still want to separate pruning refs from objects, because
rpm-ostree may have layered packages, and we only want to traverse
the ostree repo to prune objects once.

ostree-ext/ci/priv-integration.sh
@@ -58,6 +58,9 @@ for img in "${image}"; do
     test "$(($initial_refs - 1))" = "$pruned_refs"
     ostree admin --sysroot="${sysroot}" undeploy 0
     # TODO: when we fold together ostree and ostree-ext, automatically prune layers
+    n_commits=$(find ${sysroot}/ostree/repo -name '*.commit')
+    test "${n_commits}" -gt 0
+    # But right now this still doesn't prune *content*
     ostree-ext-cli container image prune-layers --repo="${sysroot}/ostree/repo"
     ostree --repo="${sysroot}/ostree/repo" refs > refs.txt
     if test "$(wc -l < refs.txt)" -ne 0; then
@@ -65,6 +68,10 @@ for img in "${image}"; do
         cat refs.txt
         exit 1
     fi
+    # And this one should GC the objects too
+    ostree-ext-cli container image prune-images --full --repo="${sysroot}/ostree/repo" > out.txt
+    n_commits=$(find ${sysroot}/ostree/repo -name '*.commit')
+    test "${n_commits}" -eq 0
 done
 
 # Verify we have systemd journal messages

ostree-ext/lib/src/cli.rs
@@ -325,6 +325,10 @@ pub(crate) enum ContainerImageOpts {
         #[clap(long)]
         /// Also prune layers
         and_layers: bool,
+
+        #[clap(long, conflicts_with = "and_layers")]
+        /// Also prune layers and OSTree objects
+        full: bool,
     },
 
     /// Perform initial deployment for a container image
@@ -1005,25 +1009,40 @@ async fn run_from_opt(opt: Opt) -> Result<()> {
                 ContainerImageOpts::PruneImages {
                     sysroot,
                     and_layers,
+                    full,
                 } => {
                     let sysroot = &ostree::Sysroot::new(Some(&gio::File::for_path(&sysroot)));
                     sysroot.load(gio::Cancellable::NONE)?;
                     let sysroot = &SysrootLock::new_from_sysroot(sysroot).await?;
-                    let removed = crate::container::deploy::remove_undeployed_images(sysroot)?;
-                    match removed.as_slice() {
-                        [] => {
-                            println!("No unreferenced images.");
-                            return Ok(());
+                    if full {
+                        let res = crate::container::deploy::prune(sysroot)?;
+                        if res.is_empty() {
+                            println!("No content was pruned.");
+                        } else {
+                            println!("Removed images: {}", res.n_images);
+                            println!("Removed layers: {}", res.n_layers);
+                            println!("Removed objects: {}", res.n_objects_pruned);
+                            let objsize = glib::format_size(res.objsize);
+                            println!("Freed: {objsize}");
                         }
-                        o => {
-                            for imgref in o {
-                                println!("Removed: {imgref}");
+                    } else {
+                        let removed = crate::container::deploy::remove_undeployed_images(sysroot)?;
+                        match removed.as_slice() {
+                            [] => {
+                                println!("No unreferenced images.");
+                                return Ok(());
+                            }
+                            o => {
+                                for imgref in o {
+                                    println!("Removed: {imgref}");
+                                }
                             }
                         }
-                    }
-                    if and_layers {
-                        let nlayers = crate::container::store::gc_image_layers(&sysroot.repo())?;
-                        println!("Removed layers: {nlayers}");
+                        if and_layers {
+                            let nlayers =
+                                crate::container::store::gc_image_layers(&sysroot.repo())?;
+                            println!("Removed layers: {nlayers}");
+                        }
                     }
                     Ok(())
                 }

ostree-ext/lib/src/container/deploy.rs
@@ -2,7 +2,7 @@
 
 use std::collections::HashSet;
 
-use super::store::LayeredImageState;
+use super::store::{gc_image_layers, LayeredImageState};
 use super::{ImageReference, OstreeImageReference};
 use crate::container::store::PrepareResult;
 use crate::keyfileext::KeyFileExt;
@@ -173,4 +173,48 @@ pub fn remove_undeployed_images(sysroot: &SysrootLock) -> Result<Vec<ImageRefere
         }
     }
     Ok(removed)
+}
+
+/// The result of a prune operation
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Pruned {
+    /// The number of images that were pruned
+    pub n_images: u32,
+    /// The number of image layers that were pruned
+    pub n_layers: u32,
+    /// The number of OSTree objects that were pruned
+    pub n_objects_pruned: u32,
+    /// The total size of pruned objects
+    pub objsize: u64,
+}
+
+impl Pruned {
+    /// Whether this prune was a no-op (i.e. no images, layers or objects were pruned).
+    pub fn is_empty(&self) -> bool {
+        self.n_images == 0 && self.n_layers == 0 && self.n_objects_pruned == 0
+    }
+}
+
+/// This combines the functionality of [`remove_undeployed_images()`] with [`super::store::gc_image_layers()`].
+pub fn prune(sysroot: &SysrootLock) -> Result<Pruned> {
+    let repo = &sysroot.repo();
+    // Prune container images which are not deployed.
+    // SAFETY: There should never be more than u32 images
+    let n_images = remove_undeployed_images(sysroot)?.len().try_into().unwrap();
+    // Prune unreferenced layer branches.
+    let n_layers = gc_image_layers(repo)?;
+    // Prune the objects in the repo; the above just removed refs (branches).
+    let (_, n_objects_pruned, objsize) = repo.prune(
+        ostree::RepoPruneFlags::REFS_ONLY,
+        0,
+        ostree::gio::Cancellable::NONE,
+    )?;
+    // SAFETY: The number of pruned objects should never be negative
+    let n_objects_pruned = u32::try_from(n_objects_pruned).unwrap();
+    Ok(Pruned {
+        n_images,
+        n_layers,
+        n_objects_pruned,
+        objsize,
+    })
 }