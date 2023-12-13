eb5faf1ec752057e83e5684f8ccbefe1d6d8fa66
Colin Walters <walters@verbum.org>
2023-12-13 14:05:35 -0500

    ci: Add a c9s build
    Prep for more testing there.

--- ci/installdeps.sh
+++ ci/installdeps.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -xeuo pipefail
 
+OS_ID=$(. /usr/lib/os-release && echo $ID)
+
+baseurl=
+case $OS_ID in
+    fedora) baseurl=https://download.copr.fedorainfracloud.org/results/@CoreOS/continuous/fedora-\$releasever-\$basearch/ ;;
+    # Default to c9s (also covers all variants/derivatives)
+    *) baseurl=https://download.copr.fedorainfracloud.org/results/@CoreOS/continuous/centos-stream-\$releasever-\$basearch/ ;;
+esac
+
 # For some reason dnf copr enable -y says there are no builds?
-cat >/etc/yum.repos.d/coreos-continuous.repo << 'EOF'
+cat >/etc/yum.repos.d/coreos-continuous.repo << EOF
 [copr:copr.fedorainfracloud.org:group_CoreOS:continuous]
 name=Copr repo for continuous owned by @CoreOS
-baseurl=https://download.copr.fedorainfracloud.org/results/@CoreOS/continuous/fedora-$releasever-$basearch/
+baseurl=$baseurl
 type=rpm-md
 skip_if_unavailable=True
 gpgcheck=1
@@ -15,11 +24,10 @@ enabled=1
 enabled_metadata=1
 EOF
 
-# Pull skopeo and ostree from updates-testing, since we depend on new features in our git main
-dnf config-manager --set-enabled updates-testing
-
 # Our tests depend on this
-dnf -y install skopeo
+dnf -y install skopeo zstd
 
-# Always pull ostree from updates-testing to avoid the bodhi wait
-dnf -y update ostree
+case $OS_ID in
+    fedora) dnf -y builddep bootc ;;
+    *) dnf -y install openssl-devel ostree-devel cargo ;;
+esac
