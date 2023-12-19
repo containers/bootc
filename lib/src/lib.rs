48dfc241e1a135cdd49daea30f140ccaa6020003
Colin Walters <walters@verbum.org>
2023-12-19 08:12:22 -0500

    lib: `#![allow(clippy::needless_borrow)]`
    In my experience this is far and away the clippy lint that is:
    
    - Most common to trigger (especially when refactoring code)
    - The least valuable to fix; there's no performance or correctness
      concerns really
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -12,6 +12,10 @@
 #![cfg_attr(feature = "dox", feature(doc_cfg))]
 #![deny(clippy::dbg_macro)]
 #![deny(clippy::todo)]
+// These two are in my experience the lints which are most likely
+// to trigger, and among the least valuable to fix.
+#![allow(clippy::needless_borrow)]
+#![allow(clippy::needless_borrows_for_generic_args)]
 
 pub mod cli;
 pub(crate) mod deploy;
