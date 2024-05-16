be7f3452d2a9bbf604ac79db8c4130c1ee4e3cf2
Xiaofeng Wang <henrywangxf@me.com>
2024-05-17 07:50:58 +0800

    test: some updates on test
    1. add bootc swtich test
    2. add bootc install to-disk test
    3. cover more distros, like rhel 9.5, fedora 40 and 41(rawhide)
    
    Signed-off-by: Xiaofeng Wang <henrywangxf@me.com>

--- tests/integration/bootc-install-upgrade.sh
+++ tests/integration/bootc-install-upgrade.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 set -exuo pipefail
 
-# Colorful timestamped output.
-function greenprint {
-    echo -e "\033[1;32m[$(date -Isecond)] ${1}\033[0m"
-}
-
-function redprint {
-    echo -e "\033[1;31m[$(date -Isecond)] ${1}\033[0m"
-}
-
-function retry {
-    n=0
-    until [ "$n" -ge 3 ]
-    do
-       "$@" && break
-       n=$((n+1))
-       sleep 10
-    done
-}
+source ./shared_lib.sh
+dump_runner
 
 TEMPDIR=$(mktemp -d)
 trap 'rm -rf -- "$TEMPDIR"' EXIT
@@ -34,14 +18,25 @@ QUAY_REPO_TAG="${QUAY_REPO_TAG:-$(tr -dc a-z0-9 < /dev/urandom | head -c 4 ; ech
 INVENTORY_FILE="${TEMPDIR}/inventory"
 
 REPLACE_CLOUD_USER=""
+TEST_IMAGE_NAME="bootc-workflow-test"
+TEST_IMAGE_URL="quay.io/redhat_emp1/${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}"
+
 case "$TEST_OS" in
-    "rhel-9-4")
-        IMAGE_NAME="rhel9-rhel_bootc"
-        TIER1_IMAGE_URL="${RHEL_REGISTRY_URL}/${IMAGE_NAME}:rhel-9.4"
+    "rhel-9"*)
+        if [[ "$TEST_OS" == "rhel-9-4" ]]; then
+            TIER1_IMAGE_URL="${RHEL_REGISTRY_URL}/rhel9-rhel_bootc:rhel-9.4"
+            BATCH_COMPOSE="updates/"
+            LATEST_COMPOSE_ID="latest-RHEL-9.4.0"
+            REDHAT_VERSION_ID="9.4"
+        else
+            TIER1_IMAGE_URL="${RHEL_REGISTRY_URL}/rhel9-rhel_bootc:rhel-9.5"
+            BATCH_COMPOSE=""
+            LATEST_COMPOSE_ID="latest-RHEL-9.5.0"
+            REDHAT_VERSION_ID="9.5"
+        fi
         SSH_USER="cloud-user"
-        CURRENT_COMPOSE_RHEL94=$(skopeo inspect --tls-verify=false "docker://${TIER1_IMAGE_URL}" | jq -r '.Labels."redhat.compose-id"')
-        sed "s/REPLACE_ME/${DOWNLOAD_NODE}/; s/REPLACE_COMPOSE_ID/${CURRENT_COMPOSE_RHEL94}/" files/rhel-9.template | tee rhel-9.repo > /dev/null
-        ADD_REPO="COPY rhel-9.repo /etc/yum.repos.d/rhel-9.repo"
+        sed "s/REPLACE_ME/${DOWNLOAD_NODE}/; s|REPLACE_BATCH_COMPOSE|${BATCH_COMPOSE}|; s/REPLACE_COMPOSE_ID/${LATEST_COMPOSE_ID}/" files/rhel-9-y.template | tee rhel-9-y.repo > /dev/null
+        ADD_REPO="COPY rhel-9-y.repo /etc/yum.repos.d/rhel-9-y.repo"
         if [[ "$PLATFORM" == "aws" ]]; then
             SSH_USER="ec2-user"
             REPLACE_CLOUD_USER='RUN sed -i "s/name: cloud-user/name: ec2-user/g" /etc/cloud/cloud.cfg'
@@ -50,27 +45,38 @@ case "$TEST_OS" in
         tee -a "playbooks/user-data" > /dev/null << EOF
 #cloud-config
 yum_repos:
-  rhel-9x-baseos:
-    name: rhel-9x-baseos
-    baseurl: http://${DOWNLOAD_NODE}/rhel-9/composes/RHEL-9/${CURRENT_COMPOSE_RHEL94}/compose/BaseOS/${ARCH}/os/
+  rhel-9y-baseos:
+    name: rhel-9y-baseos
+    baseurl: http://${DOWNLOAD_NODE}/rhel-9/nightly/${BATCH_COMPOSE}RHEL-9/${LATEST_COMPOSE_ID}/compose/BaseOS/\$basearch/os/
     enabled: true
     gpgcheck: false
-  rhel-9x-appstream:
-    name: rhel-9x-appstream
-    baseurl: http://${DOWNLOAD_NODE}/rhel-9/composes/RHEL-9/${CURRENT_COMPOSE_RHEL94}/compose/AppStream/${ARCH}/os/
+  rhel-9y-appstream:
+    name: rhel-9y-appstream
+    baseurl: http://${DOWNLOAD_NODE}/rhel-9/nightly/${BATCH_COMPOSE}RHEL-9/${LATEST_COMPOSE_ID}/compose/AppStream/\$basearch/os/
     enabled: true
     gpgcheck: false
 EOF
         ;;
     "centos-stream-9")
-        IMAGE_NAME="centos-bootc-dev"
-        TIER1_IMAGE_URL="quay.io/centos-bootc/${IMAGE_NAME}:stream9"
+        TIER1_IMAGE_URL="quay.io/centos-bootc/centos-bootc-dev:stream9"
         SSH_USER="cloud-user"
         ADD_REPO=""
         if [[ "$PLATFORM" == "aws" ]]; then
             SSH_USER="ec2-user"
             REPLACE_CLOUD_USER='RUN sed -i "s/name: cloud-user/name: ec2-user/g" /etc/cloud/cloud.cfg'
         fi
+        REDHAT_VERSION_ID="9"
+        ;;
+    "fedora"*)
+        if [[ "$TEST_OS" == "fedora-40" ]]; then
+            TIER1_IMAGE_URL="quay.io/fedora/fedora-bootc:40"
+            REDHAT_VERSION_ID="40"
+        else
+            TIER1_IMAGE_URL="quay.io/fedora/fedora-bootc:41"
+            REDHAT_VERSION_ID="41"
+        fi
+        SSH_USER="fedora"
+        ADD_REPO=""
         ;;
     *)
         redprint "Variable TEST_OS has to be defined"
@@ -78,10 +84,8 @@ EOF
         ;;
 esac
 
-TEST_IMAGE_NAME="${IMAGE_NAME}-test"
-TEST_IMAGE_URL="quay.io/redhat_emp1/${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}"
-
 sed "s/REPLACE_ME/${QUAY_SECRET}/g" files/auth.template | tee auth.json > /dev/null
+
 greenprint "Create $TEST_OS installation Containerfile"
 tee "$INSTALL_CONTAINERFILE" > /dev/null << EOF
 FROM "$TIER1_IMAGE_URL"
@@ -89,11 +93,31 @@ $ADD_REPO
 COPY build/bootc-2*.${ARCH}.rpm .
 RUN dnf -y update ./bootc-2*.${ARCH}.rpm && \
     rm -f ./bootc-2*.${ARCH}.rpm
+COPY auth.json /etc/ostree/auth.json
+EOF
+
+case "$PLATFORM" in
+    "aws")
+        tee -a "$INSTALL_CONTAINERFILE" > /dev/null << EOF
 RUN dnf -y install python3 cloud-init && \
     dnf -y clean all
-COPY auth.json /etc/ostree/auth.json
 $REPLACE_CLOUD_USER
 EOF
+        ;;
+    "libvirt")
+        SSH_USER="root"
+        SSH_KEY_PUB_CONTENT=$(cat "${SSH_KEY_PUB}")
+        tee -a "$INSTALL_CONTAINERFILE" > /dev/null << EOF
+RUN mkdir -p /usr/etc-system/ && \
+    echo 'AuthorizedKeysFile /usr/etc-system/%u.keys' >> /etc/ssh/sshd_config.d/30-auth-system.conf && \
+    echo "$SSH_KEY_PUB_CONTENT" > /usr/etc-system/root.keys && \
+    chmod 0600 /usr/etc-system/root.keys && \
+    dnf -y install qemu-guest-agent && \
+    dnf clean all && \
+    systemctl enable qemu-guest-agent
+EOF
+        ;;
+esac
 
 greenprint "Check $TEST_OS installation Containerfile"
 cat "$INSTALL_CONTAINERFILE"
@@ -129,23 +153,38 @@ EOF
 greenprint "Prepare ansible.cfg"
 export ANSIBLE_CONFIG="playbooks/ansible.cfg"
 
+# AIR_GAPPED=1 means add passthough mount to test bootc swtich to local disk
+if [[ ${AIR_GAPPED-} -eq 1 ]];then
+    AIR_GAPPED_DIR="$TEMPDIR"/virtiofs
+    mkdir "$AIR_GAPPED_DIR"
+else
+    AIR_GAPPED=0
+    AIR_GAPPED_DIR=""
+fi
+
 greenprint "Deploy $PLATFORM instance"
 ansible-playbook -v \
     -i "$INVENTORY_FILE" \
+    -e test_os="$TEST_OS" \
+    -e ssh_user="$SSH_USER" \
     -e ssh_key_pub="$SSH_KEY_PUB" \
     -e inventory_file="$INVENTORY_FILE" \
+    -e air_gapped_dir="$AIR_GAPPED_DIR" \
     "playbooks/deploy-${PLATFORM}.yaml"
 
 greenprint "Install $TEST_OS bootc system"
 ansible-playbook -v \
     -i "$INVENTORY_FILE" \
+    -e test_os="$TEST_OS" \
     -e test_image_url="$TEST_IMAGE_URL" \
     playbooks/install.yaml
 
 greenprint "Run ostree checking test on $PLATFORM instance"
 ansible-playbook -v \
     -i "$INVENTORY_FILE" \
+    -e test_os="$TEST_OS" \
     -e bootc_image="$TEST_IMAGE_URL" \
+    -e image_label_version_id="$REDHAT_VERSION_ID" \
     playbooks/check-system.yaml
 
 greenprint "Create upgrade Containerfile"
@@ -161,21 +200,32 @@ podman build --tls-verify=false --retry=5 --retry-delay=10 -t "${TEST_IMAGE_NAME
 greenprint "Push $TEST_OS upgrade container image"
 retry podman push --tls-verify=false --quiet "${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}" "$TEST_IMAGE_URL"
 
+if [[ ${AIR_GAPPED-} -eq 1 ]]; then
+    retry skopeo copy docker://"$TEST_IMAGE_URL" dir://"$AIR_GAPPED_DIR"
+    BOOTC_IMAGE="/mnt"
+else
+    BOOTC_IMAGE="$TEST_IMAGE_URL"
+fi
+
 greenprint "Upgrade $TEST_OS system"
 ansible-playbook -v \
     -i "$INVENTORY_FILE" \
+    -e air_gapped_dir="$AIR_GAPPED_DIR" \
     playbooks/upgrade.yaml
 
 greenprint "Run ostree checking test after upgrade on $PLATFORM instance"
 ansible-playbook -v \
     -i "$INVENTORY_FILE" \
-    -e bootc_image="$TEST_IMAGE_URL" \
+    -e test_os="$TEST_OS" \
+    -e bootc_image="$BOOTC_IMAGE" \
+    -e image_label_version_id="$REDHAT_VERSION_ID" \
     -e upgrade="true" \
     playbooks/check-system.yaml
 
 greenprint "Rollback $TEST_OS system"
 ansible-playbook -v \
     -i "$INVENTORY_FILE" \
+    -e air_gapped_dir="$AIR_GAPPED_DIR" \
     playbooks/rollback.yaml
 
 greenprint "Remove $PLATFORM instance"
@@ -185,7 +235,7 @@ ansible-playbook -v \
     playbooks/remove.yaml
 
 greenprint "Clean up"
-rm -rf auth.json rhel-9-4.repo
+rm -rf auth.json rhel-9-y.repo
 unset ANSIBLE_CONFIG
 
 greenprint "🎉 All tests passed."
---
+++ tests/integration/image-install-upgrade.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+set -exuo pipefail
+
+source ./shared_lib.sh
+dump_runner
+
+TEMPDIR=$(mktemp -d)
+trap 'rm -rf -- "$TEMPDIR"' EXIT
+
+# SSH configurations
+SSH_KEY=${TEMPDIR}/id_rsa
+ssh-keygen -f "${SSH_KEY}" -N "" -q -t rsa-sha2-256 -b 2048
+SSH_KEY_PUB="${SSH_KEY}.pub"
+
+INSTALL_CONTAINERFILE=${TEMPDIR}/Containerfile.install
+UPGRADE_CONTAINERFILE=${TEMPDIR}/Containerfile.upgrade
+QUAY_REPO_TAG="${QUAY_REPO_TAG:-$(tr -dc a-z0-9 < /dev/urandom | head -c 4 ; echo '')}"
+INVENTORY_FILE="${TEMPDIR}/inventory"
+
+TEST_IMAGE_NAME="bootc-workflow-test"
+
+case "$TEST_OS" in
+    "rhel-9"*)
+        if [[ "$TEST_OS" == "rhel-9-4" ]]; then
+            TIER1_IMAGE_URL="${RHEL_REGISTRY_URL}/rhel9-rhel_bootc:rhel-9.4"
+            BATCH_COMPOSE="updates/"
+            LATEST_COMPOSE_ID="latest-RHEL-9.4.0"
+            REDHAT_VERSION_ID="9.4"
+        else
+            TIER1_IMAGE_URL="${RHEL_REGISTRY_URL}/rhel9-rhel_bootc:rhel-9.5"
+            BATCH_COMPOSE=""
+            LATEST_COMPOSE_ID="latest-RHEL-9.5.0"
+            REDHAT_VERSION_ID="9.5"
+        fi
+        TEST_IMAGE_URL="quay.io/redhat_emp1/${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}"
+        SSH_USER="cloud-user"
+        sed "s/REPLACE_ME/${DOWNLOAD_NODE}/; s|REPLACE_BATCH_COMPOSE|${BATCH_COMPOSE}|; s/REPLACE_COMPOSE_ID/${LATEST_COMPOSE_ID}/" files/rhel-9-y.template | tee rhel-9-y.repo > /dev/null
+        ADD_REPO="COPY rhel-9-y.repo /etc/yum.repos.d/rhel-9-y.repo"
+        greenprint "Prepare cloud-init file"
+        tee -a "playbooks/user-data" > /dev/null << EOF
+#cloud-config
+yum_repos:
+  rhel-9y-baseos:
+    name: rhel-9y-baseos
+    baseurl: http://${DOWNLOAD_NODE}/rhel-9/nightly/${BATCH_COMPOSE}RHEL-9/${LATEST_COMPOSE_ID}/compose/BaseOS/\$basearch/os/
+    enabled: true
+    gpgcheck: false
+  rhel-9y-appstream:
+    name: rhel-9y-appstream
+    baseurl: http://${DOWNLOAD_NODE}/rhel-9/nightly/${BATCH_COMPOSE}RHEL-9/${LATEST_COMPOSE_ID}/compose/AppStream/\$basearch/os/
+    enabled: true
+    gpgcheck: false
+EOF
+        BOOT_ARGS="uefi"
+        ;;
+    "centos-stream-9")
+        TIER1_IMAGE_URL="quay.io/centos-bootc/centos-bootc-dev:stream9"
+        ADD_REPO=""
+        SSH_USER="cloud-user"
+        REDHAT_VERSION_ID="9"
+        TEST_IMAGE_URL="quay.io/bootc-test/${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}"
+        BOOT_ARGS="uefi,firmware.feature0.name=secure-boot,firmware.feature0.enabled=no"
+        ;;
+    "fedora"*)
+        if [[ "$TEST_OS" == "fedora-40" ]]; then
+            TIER1_IMAGE_URL="quay.io/fedora/fedora-bootc:40"
+            REDHAT_VERSION_ID="40"
+        else
+            TIER1_IMAGE_URL="quay.io/fedora/fedora-bootc:41"
+            REDHAT_VERSION_ID="41"
+        fi
+        SSH_USER="fedora"
+        ADD_REPO=""
+        TEST_IMAGE_URL="quay.io/bootc-test/${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}"
+        BOOT_ARGS="uefi"
+        ;;
+    *)
+        redprint "Variable TIER1_IMAGE_URL is not supported"
+        exit 1
+        ;;
+esac
+
+sed "s/REPLACE_ME/${QUAY_SECRET}/g" files/auth.template | tee auth.json > /dev/null
+greenprint "Create $TEST_OS installation Containerfile"
+tee "$INSTALL_CONTAINERFILE" > /dev/null << EOF
+FROM "$TIER1_IMAGE_URL"
+$ADD_REPO
+COPY build/bootc-2*.${ARCH}.rpm .
+RUN dnf -y update ./bootc-2*.${ARCH}.rpm && \
+    rm -f ./bootc-2*.${ARCH}.rpm
+RUN dnf -y install python3 cloud-init && \
+    dnf -y clean all
+COPY auth.json /etc/ostree/auth.json
+EOF
+
+greenprint "Check $TEST_OS installation Containerfile"
+cat "$INSTALL_CONTAINERFILE"
+
+greenprint "Login quay.io"
+sudo podman login -u "${QUAY_USERNAME}" -p "${QUAY_PASSWORD}" quay.io
+
+greenprint "Build $TEST_OS installation container image"
+sudo podman build --tls-verify=false --retry=5 --retry-delay=10 -t "${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}" -f "$INSTALL_CONTAINERFILE" .
+
+greenprint "Push $TEST_OS installation container image"
+sudo podman push --tls-verify=false --quiet "${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}" "$TEST_IMAGE_URL"
+
+greenprint "Prepare inventory file"
+tee -a "$INVENTORY_FILE" > /dev/null << EOF
+[cloud]
+localhost
+
+[guest]
+
+[cloud:vars]
+ansible_connection=local
+
+[guest:vars]
+ansible_user="$SSH_USER"
+ansible_private_key_file="$SSH_KEY"
+ansible_ssh_common_args="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+[all:vars]
+ansible_python_interpreter=/usr/bin/python3
+EOF
+
+greenprint "Prepare ansible.cfg"
+export ANSIBLE_CONFIG="${PWD}/playbooks/ansible.cfg"
+
+case "$IMAGE_TYPE" in
+    "to-disk")
+        greenprint "Configure rootfs randomly"
+        ROOTFS_LIST=( \
+            "ext4" \
+            "xfs" \
+        )
+        RND_LINE=$((RANDOM % 2))
+        ROOTFS="${ROOTFS_LIST[$RND_LINE]}"
+
+        if [[ "$TEST_OS" == "fedora"* ]]; then
+            ROOTFS="btrfs"
+        fi
+
+        greenprint "💾 Create disk.raw"
+        sudo truncate -s 10G disk.raw
+
+        greenprint "bootc install to disk.raw"
+        sudo podman run \
+            --rm \
+            --privileged \
+            --pid=host \
+            --security-opt label=type:unconfined_t \
+            -v /var/lib/containers:/var/lib/containers \
+            -v /dev:/dev \
+            -v .:/output \
+            "$TEST_IMAGE_URL" \
+            bootc install to-disk --filesystem "$ROOTFS" --generic-image --via-loopback /output/disk.raw
+
+        sudo qemu-img convert -f raw ./disk.raw -O qcow2 "/var/lib/libvirt/images/disk.qcow2"
+        rm -f disk.raw
+
+        if [[ "$ARCH" == "x86_64" ]]; then
+            BIB_FIRMWARE_LIST=( \
+                "bios" \
+                "uefi" \
+            )
+            RND_LINE=$((RANDOM % 2))
+            BIB_FIRMWARE="${BIB_FIRMWARE_LIST[$RND_LINE]}"
+        else
+            BIB_FIRMWARE="uefi"
+        fi
+
+        greenprint "Deploy $IMAGE_TYPE instance"
+        ansible-playbook -v \
+            -i "$INVENTORY_FILE" \
+            -e test_os="$TEST_OS" \
+            -e ssh_key_pub="$SSH_KEY_PUB" \
+            -e ssh_user="$SSH_USER" \
+            -e inventory_file="$INVENTORY_FILE" \
+            -e bib="true" \
+            -e boot_args="$BOOT_ARGS" \
+            -e bib_firmware="$BIB_FIRMWARE" \
+            "playbooks/deploy-libvirt.yaml"
+        ;;
+    *)
+        redprint "Variable IMAGE_TYPE has to be defined"
+        exit 1
+        ;;
+esac
+
+greenprint "Run ostree checking test on $PLATFORM instance"
+ansible-playbook -v \
+    -i "$INVENTORY_FILE" \
+    -e test_os="$TEST_OS" \
+    -e bootc_image="$TEST_IMAGE_URL" \
+    -e image_label_version_id="$REDHAT_VERSION_ID" \
+    playbooks/check-system.yaml
+
+greenprint "Create upgrade Containerfile"
+tee "$UPGRADE_CONTAINERFILE" > /dev/null << EOF
+FROM "$TEST_IMAGE_URL"
+RUN dnf -y install wget && \
+    dnf -y clean all
+EOF
+
+greenprint "Build $TEST_OS upgrade container image"
+sudo podman build --tls-verify=false --retry=5 --retry-delay=10 -t "${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}" -f "$UPGRADE_CONTAINERFILE" .
+greenprint "Push $TEST_OS upgrade container image"
+sudo podman push --tls-verify=false --quiet "${TEST_IMAGE_NAME}:${QUAY_REPO_TAG}" "$TEST_IMAGE_URL"
+
+greenprint "Upgrade $TEST_OS system"
+ansible-playbook -v \
+    -i "$INVENTORY_FILE" \
+    playbooks/upgrade.yaml
+
+greenprint "Run ostree checking test after upgrade on $PLATFORM instance"
+ansible-playbook -v \
+    -i "$INVENTORY_FILE" \
+    -e test_os="$TEST_OS" \
+    -e bootc_image="$TEST_IMAGE_URL" \
+    -e image_label_version_id="$REDHAT_VERSION_ID" \
+    -e upgrade="true" \
+    playbooks/check-system.yaml
+
+greenprint "Rollback $TEST_OS system"
+ansible-playbook -v \
+    -i "$INVENTORY_FILE" \
+    playbooks/rollback.yaml
+
+greenprint "Terminate $PLATFORM instance and deregister AMI"
+ansible-playbook -v \
+    -i "$INVENTORY_FILE" \
+    -e platform="$PLATFORM" \
+    playbooks/remove.yaml
+
+greenprint "Clean up"
+rm -rf auth.json rhel-9-y.repo
+unset ANSIBLE_CONFIG
+
+greenprint "🎉 All tests passed."
+exit 0
