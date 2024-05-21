0f8f841191ebf13bad9983d8dcd47a40fc18d1a5
John Eckersberg <jeckersb@redhat.com>
2024-05-21 14:27:33 -0400

    lib: Run `cargo fmt`
    Same as #547, but I'm not sure how this instance wasn't caught
    previously.
    
    Signed-off-by: John Eckersberg <jeckersb@redhat.com>

--- lib/src/bootloader.rs
+++ lib/src/bootloader.rs
@@ -20,11 +20,7 @@ pub(crate) fn install_via_bootupd(
         .into_iter()
         .chain(verbose)
         .chain(bootupd_opts.iter().copied().flatten())
-        .chain([
-            "--device",
-            device.as_str(),
-            rootfs.as_str(),
-        ]);
+        .chain(["--device", device.as_str(), rootfs.as_str()]);
     Task::new("Running bootupctl to install bootloader", "bootupctl")
         .args(args)
         .verbose()
