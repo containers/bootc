0455f808b12e587dbf7700a8468fb984b2c78b3e
Colin Walters <walters@verbum.org>
2024-06-04 14:10:01 -0400

    build: Fetch latest spec from Fedora
    To get `zstd-devel`.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- hack/build.sh
+++ hack/build.sh
@@ -5,4 +5,8 @@ case $ID in
   centos|rhel) dnf config-manager --set-enabled crb;;
   fedora) dnf -y install dnf-utils ;;
 esac
-dnf -y builddep bootc
+# Fetch the latest spec from fedora to ensure we've got the latest build deps
+t=$(mktemp --suffix .spec)
+curl -L -o ${t} https://src.fedoraproject.org/rpms/bootc/raw/rawhide/f/bootc.spec
+dnf -y builddep "${t}"
+rm -f "${t}"
