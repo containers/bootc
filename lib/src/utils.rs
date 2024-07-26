bbd63314a38827227c0db3a3fa49c58fe72cf4f1
Colin Walters <walters@verbum.org>
2024-07-26 14:56:41 -0400

    utils: Allow non-static str for spinner
    I have a use case for dynamic strings, this just requires
    duplicating the string for the progress bar.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- lib/src/utils.rs
+++ lib/src/utils.rs
@@ -132,14 +132,14 @@ pub(crate) fn medium_visibility_warning(s: &str) {
 /// with an automatic spinner to show that we're not blocked.
 /// Note that generally the called function should not output
 /// anything to stdout as this will interfere with the spinner.
-pub(crate) async fn async_task_with_spinner<F, T>(msg: &'static str, f: F) -> T
+pub(crate) async fn async_task_with_spinner<F, T>(msg: &str, f: F) -> T
 where
     F: Future<Output = T>,
 {
     let pb = indicatif::ProgressBar::new_spinner();
     let style = indicatif::ProgressStyle::default_bar();
     pb.set_style(style.template("{spinner} {msg}").unwrap());
-    pb.set_message(msg);
+    pb.set_message(msg.to_string());
     pb.enable_steady_tick(Duration::from_millis(150));
     // We need to handle the case where we aren't connected to
     // a tty, so indicatif would show nothing by default.
