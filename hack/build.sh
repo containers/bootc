8990b54244bdb005f67abd6bf3b422c33bb4f6af
Colin Walters <walters@verbum.org>
2025-01-17 09:54:35 -0500

    build.sh: Use our embedded spec
    One CI run just got a server error fetching the spec from Fedora.
    We have a spec here (which I don't like but we do) so use it
    to lower CI flakes.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- hack/build.sh
+++ hack/build.sh
@@ -5,10 +5,6 @@ case $ID in
   centos|rhel) dnf config-manager --set-enabled crb;;
   fedora) dnf -y install dnf-utils ;;
 esac
-# Fetch the latest spec from fedora to ensure we've got the latest build deps
-t=$(mktemp --suffix .spec)
-curl -L -o ${t} https://src.fedoraproject.org/rpms/bootc/raw/rawhide/f/bootc.spec
-dnf -y builddep "${t}"
-rm -f "${t}"
+dnf -y builddep ./contrib/packaging/bootc.spec
 # Extra dependencies
 dnf -y install git-core
