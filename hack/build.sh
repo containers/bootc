292d9e165c8536dd6f6b58e9a2caf508f60fe98b
Colin Walters <walters@verbum.org>
2025-02-01 10:34:07 -0500

    build: Handle dnf5 in Fedora
    Signed-off-by: Colin Walters <walters@verbum.org>

--- hack/build.sh
+++ hack/build.sh
@@ -3,7 +3,7 @@ set -xeu
 . /usr/lib/os-release
 case $ID in
   centos|rhel) dnf config-manager --set-enabled crb;;
-  fedora) dnf -y install dnf-utils ;;
+  fedora) dnf -y install dnf-utils 'dnf5-command(builddep)';;
 esac
 dnf -y builddep ./contrib/packaging/bootc.spec
 # Extra dependencies
